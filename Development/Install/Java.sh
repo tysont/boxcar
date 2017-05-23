e38a45e48578f0d779dff208cf0f4243f5a567e6
Tyson Trautmann <tysont@gmail.com>
2017-05-22 21:58:03 -0700

    Updating java version.

--- Development/Install/Java.sh
+++ Development/Install/Java.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ "$OSTYPE" = "cygwin" ]; then (set -o igncr) 2>/dev/null && set -o igncr; fi # Cygwin CRLF fix
 	
-wget --no-cookies --no-check-certificate --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8-b132/jdk-8-linux-x64.tar.gz
-tar -C /opt -xzf jdk-8-linux-x64.tar.gz
-ln -s /opt/jdk1.8.0/bin/* /usr/local/bin
+wget --no-cookies --no-check-certificate --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz
+tar -C /opt -xzf jdk-8u131-linux-x64.tar.gz
+ln -s /opt/jdk1.8.0_131/bin/* /usr/local/bin
