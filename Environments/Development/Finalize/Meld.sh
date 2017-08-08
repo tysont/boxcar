2766829e8c77081c52a5e73d9edd1be578dbef25
Tyson Trautmann <tysont@gmail.com>
2017-08-08 22:59:54 +0100

    Adding script to make meld default git difftool.

---
+++ Environments/Development/Finalize/Meld.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+if [ "$OSTYPE" = "cygwin" ]; then (set -o igncr) 2>/dev/null && set -o igncr; fi # Cygwin CRLF fix
+
+git config --global diff.tool meld
+git config --global difftool.prompt false
