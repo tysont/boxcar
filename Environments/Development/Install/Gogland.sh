79f98ff7475c92657097a7a6d6404f2d4822b570
Tyson Trautmann <tysont@gmail.com>
2017-08-12 07:56:25 -0700

    Fixing Gogland.

--- Environments/Development/Install/Gogland.sh
+++ Environments/Development/Install/Gogland.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 if [ "$OSTYPE" = "cygwin" ]; then (set -o igncr) 2>/dev/null && set -o igncr; fi # Cygwin CRLF fix
 
-wget https://download.jetbrains.com/idea/ideaIC-2017.1.3-no-jdk.tar.gz
-tar -C /opt -xzf ideaIC-2017.1.3-no-jdk.tar.gz
-ln -s /opt/idea-IC-171.4424.56/bin/idea.sh /usr/bin/idea
+wget https://download.jetbrains.com/go/gogland-172.3757.46.tar.gz
+tar -C /opt -xzf gogland-172.3757.46.tar.gz
+ln -s /opt/Gogland-172.3757.46/bin/gogland.sh /usr/bin/gogland
+
+
