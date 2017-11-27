96076b0562bd4e7d811e6e3d7cb54bbd0a8afebf
Tyson Trautmann <tysont@gmail.com>
2017-11-26 23:08:36 -0500

    Fixing docker deb file name.

--- Environments/Development/Install/Docker.sh
+++ Environments/Development/Install/Docker.sh
@@ -11,7 +11,7 @@ apt-get update
 #apt-get -y install docker-ce
 
 wget https://download.docker.com/linux/ubuntu/dists/yakkety/pool/stable/amd64/docker-ce_17.03.2~ce-0~ubuntu-yakkety_amd64.deb
-dpkg -i docker-ce_17.03.2_ce-0_ubuntu-yakkety_amd64.deb
+dpkg -i docker-ce_17.03.2~ce-0~ubuntu-yakkety_amd64.deb
 
 apt-get -y install python-pip
 pip install docker-compose
