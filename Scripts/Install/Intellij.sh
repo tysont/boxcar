d71c1af615c84433a4519aa74a38518fb56849ef
Tyson Trautmann <tysont@amazon.com>
2018-08-26 14:39:15 -0700

    Updating intellij download link.

--- Scripts/Install/Intellij.sh
+++ Scripts/Install/Intellij.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ "$OSTYPE" = "cygwin" ]; then (set -o igncr) 2>/dev/null && set -o igncr; fi # Cygwin CRLF fix
 
-wget https://download.jetbrains.com/idea/ideaIC-2017.3.4-no-jdk.tar.gz
-tar -C /opt -xzf ideaIC-2017.3.4-no-jdk.tar.gz
+wget https://download.jetbrains.com/idea/ideaIU-2018.2.2-no-jdk.tar.gz
+tar -C /opt -xzf ideaIU-2018.2.2-no-jdk.tar.gz
 ln -s /opt/idea-IC-173.4548.28/bin/idea.sh /usr/bin/idea
